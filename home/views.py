fb7ffddef7e8ced2807810e0e9dd991c392000b3
arvind <alwen1928@gmail.com>
2022-02-03 14:17:56 +0530

    initial commit

---
+++ home/views.py
@@ -0,0 +1,11 @@
+from django import template
+from django.template import loader
+from django.urls import reverse
+from django.shortcuts import render
+def index(request):
+   
+    return render(request,'index-3.html')
+
+
+    
+   
