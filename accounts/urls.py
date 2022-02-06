453bebd8f252e11552c6b8275f883746f381fd5f
arvind <alwen1928@gmail.com>
2022-02-06 13:16:20 +0530

    displayed username when logged in successful

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,14 +1,15 @@
 
 
 from django.urls import path
-from .views import login_view
+from .views import *
 from django.contrib.auth.views import LogoutView
 from accounts import views
 from .views import register_request
 
 urlpatterns = [
-    
     path('login/', login_view, name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
-    path("register/", views.register_request, name="register")
+    path("register/", views.register_request, name="register"),
+    
+    
 ]
